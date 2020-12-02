4cbdc6cdb9ad78d1c93bd64cbd5bae21700b677c
yeisongmz <74453055+yeisongmz@users.noreply.github.com>
2020-12-02 00:21:23 -0300

    dqwd dqedeq

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 var mascotas = [
-  { tipo: "perro", nombre: "kukis", active: false },
-  { tipo: "gato", nombre: "michi", active: false },
-  { tipo: "conejo", nombre: "Gudú", active: false }
+  { tipo: "perro", nombre: "kukis", active: false, dueño: "hernan" },
+  { tipo: "gato", nombre: "michi", active: true, dueño: "deisy" },
+  { tipo: "conejo", nombre: "marrón", active: false, dueño: "jose" },
+  { tipo: "perro", nombre: "kukis", active: true, dueño: "angel" },
+  { tipo: "gato", nombre: "blanco", active: true, dueño: "matias" },
+  { tipo: "mono", nombre: "negro", active: true, dueño: "marian" },
+  { tipo: "perro", nombre: "kukis", active: false, dueño: "matias" },
+  { tipo: "mono", nombre: "tyson", active: true, dueño: "jose" },
+  { tipo: "conejo", nombre: "marrón", active: false, dueño: "marian" },
+  { tipo: "mono", nombre: "yeison", active: true, dueño: "victor" },
+  { tipo: "gato", nombre: "blanco", active: true, dueño: "jose" },
+  { tipo: "mono", nombre: "chango", active: false, dueño: "angel" }
 ];
-
+/*
 let buscarGato = _.findIndex(mascotas, function (o) {
   return o.nombre == "mich" && o.active;
 });
@@ -22,3 +31,30 @@ let buscarMascotaPorTipo = _.findIndex(mascotas, function (o) {
 
 console.log("buscar mascota por tipo");
 console.log("se encontró la mascota " + buscarMascotaPorTipo);
+
+console.log("vamos a filtrar las mascotas");
+
+let filtrarGatos = _.filter(mascotas, function (o) {
+  return o.tipo === "gato";
+});
+
+console.log("filtramos los gatos");
+
+console.log("Esta es la lista de gatos");
+console.log(filtrarGatos);
+*/
+/*
+console.log("filtrar monos activos");
+let filtrarMonosActivos = _.filter(mascotas, function(o){
+  return o.tipo=="mono" && o.active==true;
+});
+
+
+console.log("lista de monos activos");
+console.log(filtrarMonosActivos);*/
+
+//tarea
+
+//filtrar mascotas de deisy activoas
+
+//filtrar monos de matias activos
